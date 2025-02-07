af32db89d86f5519f792a8a17d6ce2a51f10c1ed
0097eo <emmanuelokello294@gmail.com>
2025-02-07 10:01:49 +0300

    add testing setup with vitest and testing-library

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test/setup.js'],
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      reporter: ['text', 'json', 'html'],
+    }
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5555',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '/'),
       }
     }
   }
